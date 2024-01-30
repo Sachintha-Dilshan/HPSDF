e0da8d515dba4861980d9f8703ea8f4625e56042
Hirushan99 <palihewadana11716@usci.ruh.ac.lk>
2024-01-31 00:18:09 +0530

    sss

--- Frontend/src/Components/ARC/SearchArea.js
+++ Frontend/src/Components/ARC/SearchArea.js
@@ -8,7 +8,7 @@ function SearchArea() {
   return (
     <div className="mt-8 grid lg:grid-cols-2 sm:grid-cols-1  gap-y-10"> 
         <div>
-            <span className="overflow-hidden text-ellipsis whitespace-nowrap font-Outfit text-[#174082] text-xl font-bold  leading-7.5">FILE NUMBER</span>
+            <span className="overflow-hidden text-ellipsis whitespace-nowrap  font-Outfit text-[#174082] text-xl font-bold  leading-7.5">FILE NUMBER</span>
             <BlueLine className="mt-1"/>
             <TextInput className="w-80 mt-3"/>
        </div>
