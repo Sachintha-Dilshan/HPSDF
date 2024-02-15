5f71a08d8174ad0bee07d55c7b4d04d1c0aa0913
Hirushan99 <palihewadana11716@usci.ruh.ac.lk>
2024-02-15 11:26:44 +0530

    ar-frontend-5-pages-hirushan

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import PageLayout from "./layouts/page-layout";
-import HRSearchEmployees from "./hr/pages/hr-search-employees";
+import { Route, Routes } from "react-router-dom";
+import HRLeaveAddLeaves from "./hr/leave/pages/hr-leave-add-leaves";
+import HREmployeesAttendanceSheet from "./hr/leave/pages/hr-leave-attendance-sheet";
 import HRAddEmployee from "./hr/pages/hr-add-employee";
+import HRDashboard from "./hr/pages/hr-dashboard";
 import HREmployeeProfile from "./hr/pages/hr-employee-profile";
-//import HRDashboard from "./hr/pages/hr-dashboard";
 import HREmployeeLeaveRequest from "./hr/pages/hr-leave-request";
 import HROnLeaveToday from "./hr/pages/hr-on-leave-today";
-import HRLeaveDashboard from "./hr/leave/pages/hr-leave-dashboard";
-import HREmployeesAttendanceSheet from "./hr/leave/pages/hr-leave-attendance-sheet";
-import HRLeaveAddLeaves from "./hr/leave/pages/hr-leave-add-leaves";
-import Login from "./pages/login";
+import HRSearchEmployees from "./hr/pages/hr-search-employees";
+import PageLayout from "./layouts/page-layout";
 import NoPage from "./pages/no-page";
-
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        {/*<Route path="/" element={<Login/>}/>*/}
         <Route path="/" element={<PageLayout />}>
           <Route path="HR">
-            <Route path="dashboard" element={<HRLeaveDashboard />} />
+            <Route path="dashboard" element={<HRDashboard/>} />
             <Route path="allEmployees" element={<HRSearchEmployees />} />
             <Route path="employeeProfile" element={<HREmployeeProfile />}/>
             <Route path="addEmployee" element={<HRAddEmployee />} />    
